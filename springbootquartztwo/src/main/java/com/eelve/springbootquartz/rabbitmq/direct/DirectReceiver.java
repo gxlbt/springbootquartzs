57a7aec1f7b0adebbe466bc1818f863c2790c769
lbt <eamil@qq.com>
2020-01-07 18:02:23 +0800

    rabbitmq初探

---
+++ springbootquartztwo/src/main/java/com/eelve/springbootquartz/rabbitmq/direct/DirectReceiver.java
@@ -0,0 +1,23 @@
+package com.eelve.springbootquartz.rabbitmq.direct;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * 监听的队列名称 TestDirectQueue
+ * @author lbt
+ */
+
+@Component
+@RabbitListener(queues = "TestDirectQueue")
+public class DirectReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+
+}
